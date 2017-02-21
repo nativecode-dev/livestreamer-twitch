4067401a5ad4e009c97d88ab08edfecf0aeaaabd
Mike Pham <contact@mikepham.com>
2017-02-20 19:31:24 -0500

    updating package name and removing docker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const locations = require('common-locations')('livestreamer')
 const twitch = require('twitch.tv')
 
 const TwitchStreamer = require('livestreamer').TwitchStreamer
-const configpath = locations.config.local('livestreamer-channels.json')
+const configpath = locations.config.local('livestreamer.json')
 
 let running = {}
 let ignored = []
@@ -16,19 +16,33 @@ const available = (config, channel) => {
 
     console.log('streaming: %s', channel)
 
-    running[channel] = new TwitchStreamer(channel).start(options.quality).then(() => {
-      debug('completed streaming -> %s', channel)
-      delete running[channel]
-      console.log('completed: %s', channel)
-    }).catch(() => {
-      debug('error while streaming -> %s', channel)
-      delete running[channel]
-      console.log('errored: %s', channel)
-    })
+    running[channel] = new TwitchStreamer(channel, config.livestreamer)
+      .start(options.quality)
+      .then(() => {
+        debug('completed streaming -> %s', channel)
+        delete running[channel]
+        console.log('completed: %s', channel)
+      }).catch(() => {
+        debug('error while streaming -> %s', channel)
+        delete running[channel]
+        console.log('errored: %s', channel)
+      })
 
   } else {
     debug('channel already running -> %s', channel)
   }
+}
+
+const filter = (channel, config, stream) => {
+  return config.ignore.keywords.some(keyword => {
+    if (stream.channel.status.toLowerCase().includes(keyword.toLowerCase())) {
+      debug('[%s] ignoring keyword -> %s', channel, keyword)
+      return true
+    }
+
+    debug('[%s] keyword not found -> %s', channel, keyword)
+    return false
+  })
 }
 
 const scan = () => {
@@ -41,21 +55,23 @@ const scan = () => {
         twitch(`streams/${channel}`, config.twitch.api, (error, response) => {
           if (error) {
             ignored.push(channel)
-            debug('ignoring -> %s', channel)
+            debug('error -> %s', channel)
             return
           }
 
-          if (response.stream) {
+          if (response.stream && filter(channel, config.twitch, response.stream) === false) {
             available(config, channel)
           }
-
         })
       }
     })
+  } else {
+    console.log('No configuration found at %s.', configpath)
   }
 
   debug('checking again in 30 seconds')
   setTimeout(scan, 30000)
 }
 
+console.log('starting livestreamer watch')
 scan()
